044045d236559f76515a07480c92ce4f508e377e
indextrown <indextrown@gmail.com>
2026-01-20 02:20:50 +0900

    Initial Commit

---
+++ cicd_base/cicd_baseApp.swift
@@ -0,0 +1,17 @@
+//
+//  cicd_baseApp.swift
+//  cicd_base
+//
+//  Created by 김동현 on 1/20/26.
+//
+
+import SwiftUI
+
+@main
+struct cicd_baseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
