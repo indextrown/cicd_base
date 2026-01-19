72ad98bf1e7959ce13e0a38db710371ce1ca3c6c
indextrown <indextrown@gmail.com>
2026-01-20 03:17:50 +0900

    ci: unit test 추가

---
+++ cicd_baseUnitTests/cicd_baseUnitTests.swift
@@ -0,0 +1,16 @@
+//
+//  cicd_baseUnitTests.swift
+//  cicd_baseUnitTests
+//
+//  Created by 김동현 on 1/20/26.
+//
+
+import Testing
+
+struct cicd_baseUnitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
